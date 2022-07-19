265e64bf896b9ead59207d2550cd643ce361eed6
oswaldo garcia <garcia.oswaldo1000@gmail.com>
2022-07-19 13:14:06 -0500

    Initial commit

---
+++ src/main/java/com/practicehtml/practicehtml/PracticehtmlApplication.java
@@ -0,0 +1,13 @@
+package com.practicehtml.practicehtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticehtmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticehtmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practicehtml/practicehtml/PracticehtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicehtml.practicehtml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticehtmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
